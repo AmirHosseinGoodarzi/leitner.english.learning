7387e343ebd2e23a737c18c37d29b099510b5476
AmirGoodarzi <amirgoodarzi.web@gmail.com>
2023-09-06 19:58:47 +0330

    fix meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import "./globals.scss";
-import type { Metadata } from "next";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeContextProvider from "@/contexts/themeContext";
 import AuthContextProvider from "@/contexts/authContext";
@@ -9,11 +8,6 @@ import { Toaster } from "react-hot-toast";
 import Navbar from "@/components/navbar";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import SidebarContextProvider from "@/contexts/sidebarContext";
-
-export const metadata: Metadata = {
-  title: "Learn English",
-  description: "Rapidly Learning English with Leitner's method.",
-};
 
 export default function RootLayout({
   children,
@@ -29,6 +23,14 @@ export default function RootLayout({
   });
   return (
     <html lang="en">
+      <head>
+        <title>Learn English</title>
+        <link rel="shortcut icon" href="./favicon.png" type="image/x-icon" />
+        <meta
+          name="description"
+          content="Rapidly Learning English with Leitner's method."
+        />
+      </head>
       <QueryClientProvider client={queryClient}>
         <ThemeContextProvider>
           <AuthContextProvider>
