5706029977319cbc6636f8e3ccd99735577f61b1
AmirGoodarzi <amirgoodarzi.web@gmail.com>
2023-09-06 11:57:26 +0330

    convert flippingCard styles to module

--- components/flippingCard/index.tsx
+++ components/flippingCard/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import "./flippingCard.scss";
+import styles from "./flippingCard.module.scss";
 import { WordType } from "@/utils/types";
 import ROUTES_OBJECT from "@/utils/RoutesObject";
 import { useSpeechSynthesis } from "react-speech-kit";
 import Link from "next/link";
-import { Pen, Speaker, VolumeMuteFill, VolumeUpFill } from "react-bootstrap-icons";
+import { Pen, VolumeMuteFill, VolumeUpFill } from "react-bootstrap-icons";
 
 const FlippingCard = ({ word }: { word: WordType }) => {
   const [cardFlipped, setCardFlipped] = useState(false);
@@ -23,14 +23,18 @@ const FlippingCard = ({ word }: { word: WordType }) => {
 
   return (
     <div
-      className={`flip_container cursor-pointer relative w-80 h-44 lg:w-96 lg:h-52 select-none ${
-        cardFlipped ? "flipped" : ""
+      className={`${
+        styles.flip_container
+      } cursor-pointer relative w-80 h-44 lg:w-96 lg:h-52 select-none ${
+        cardFlipped ? styles.flipped : ""
       }`}
       onClick={() => {
         setCardFlipped(!cardFlipped);
       }}
     >
-      <div className="flip_side front relative font-bold flex items-center justify-center text-3xl text-center">
+      <div
+        className={`${styles.front} relative font-bold flex items-center justify-center text-3xl text-center`}
+      >
         {word.front}
         <div className="absolute top-2 right-2 flex items-center justify-center gap-2">
           <Link href={ROUTES_OBJECT.configCard + "/" + word._id}>
@@ -57,7 +61,7 @@ const FlippingCard = ({ word }: { word: WordType }) => {
           <></>
         )}
       </div>
-      <div className="flip_side back text-justify whitespace-pre-line">
+      <div className={`${styles.back} text-justify whitespace-pre-line`}>
         {word.back}
       </div>
     </div>
