5080a006c1d854f9cdf0df7849c4c959bb72d9d6
AmirGoodarzi <amirgoodarzi.web@gmail.com>
2023-09-06 11:42:30 +0330

    Completed cards page

--- app/(dashboard)/completed/page.tsx
+++ app/(dashboard)/completed/page.tsx
@@ -1,4 +1,46 @@
+"use client";
+
+import PageHeader from "@/components/pageHeader";
+import { useGetAllWords } from "@/services/Words";
+import Image from "next/image";
+
 export default function Completed() {
-    return <div>Completed</div>;
-  }
-  
+  const { data: words, isLoading: wordsLoading } = useGetAllWords();
+  return (
+    <div className="relative pb-10">
+      <PageHeader
+        hint="Storage"
+        title="Completed Cards"
+        description="All the cards that you'd completed them"
+      />
+      <br />
+      {wordsLoading ? (
+        // <CardsLoading />
+        <p>loading</p>
+      ) : words.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center gap-5">
+          {/* {words.map((word: WordType, index: number) => {
+            return <SimpleFlippingCard word={word} key={index} />;
+          })} */}
+          ssss
+        </div>
+      ) : (
+        <div>
+          <Image
+            src="./assets/images/empty.svg"
+            width={250}
+            height={250}
+            alt="..."
+            className="mx-auto mb-5"
+          />
+          <p className="text-center">
+            You haven't complete any words yet 😔,
+            <br />
+            Add new words to your vocabulary storage <br />
+            and <br /> visit the application everyday.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
